e597dbbf3728cb25d979abc8345816f89fa767ec
mrochedy <106345479+mrochedy@users.noreply.github.com>
2025-03-05 19:58:38 +0100

    Add main.py to setup and initialize the FastAPI app

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.core import engine, Base
+from src.api import notes_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Flick API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(notes_router)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Flick API"}
