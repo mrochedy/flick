8a5f266b26583e7996bb08018a49a75223f1cd37
mrochedy <106345479+mrochedy@users.noreply.github.com>
2025-03-05 15:46:44 +0100

    Create Note model

---
+++ backend/src/models/note.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Text, DateTime
+
+from src.core import Base
+
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
