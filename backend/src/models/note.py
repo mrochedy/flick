40319a02d5a562ed07a4c907266f65026f04a66a
mrochedy <106345479+mrochedy@users.noreply.github.com>
2025-03-07 19:24:26 +0100

    Fix "updated_at" field not updating

--- backend/src/models/note.py
+++ backend/src/models/note.py
@@ -10,5 +10,5 @@ class Note(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(100), nullable=False)
     content = Column(Text, nullable=False)
-    created_at = Column(DateTime, default=datetime.now())
-    updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
