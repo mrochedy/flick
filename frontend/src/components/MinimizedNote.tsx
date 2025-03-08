23574cf51ef25c7cb28d38d28514e48e5618110e
mrochedy <106345479+mrochedy@users.noreply.github.com>
2025-03-08 16:52:07 +0100

    Make the MinimizedNote component

--- frontend/src/components/MinimizedNote.tsx
+++ frontend/src/components/MinimizedNote.tsx
@@ -1,5 +1,18 @@
-function MinimizedNote() {
-  return <div>MinimizedNote</div>;
+import MinimizedNoteTitle from "./MinimizedNoteTitle";
+import MinimizedNoteContent from "./MinimizedNoteContent";
+
+interface Props {
+  title: string;
+  content: string;
+}
+
+function MinimizedNote({ title, content }: Props) {
+  return (
+    <>
+      <MinimizedNoteTitle title={title} />
+      <MinimizedNoteContent content={content} />
+    </>
+  );
 }
 
 export default MinimizedNote;
